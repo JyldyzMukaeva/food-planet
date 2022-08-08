73fbe35f62b7aeea886b8ce6b5ed1634dd8da8a0
JyldyzMukaeva <mukaeva.jyldyz@gmail.com>
2022-08-08 11:46:20 +0600

    feat: new styles added.

--- src/pages/MainPage/components/Burgers/Burgers.js
+++ src/pages/MainPage/components/Burgers/Burgers.js
@@ -3,16 +3,15 @@ import styles from "./Burgers.module.css"
 import {burgersArray} from "../../../../constants";
 
 
-
 const Burgers = () => {
 
 
-        const burgers = burgersArray.map(item => (
+    const burgers = burgersArray.map(item => (
             <div className={styles.burgerBox}>
-                 <img src={item.image} alt=""/>
-                 <h1>{item.name}</h1>
+                <img src={item.image} alt=""/>
+                <h1>{item.name}</h1>
                 <p>{item.text}</p>
-                 <p>{item.price}<span>сом</span></p>
+                <p>{item.price}<span>сом</span></p>
                 <div className={styles.btn1}>
                     <button>-</button>
                     <span>1</span>
@@ -21,15 +20,14 @@ const Burgers = () => {
                 <div className={styles.btn2}>
                     <button>В КОРЗИНУ</button>
                 </div>
-
             </div>
-            )
-        );
-return (
-    <div className={styles.burgers}>
-        {burgers}
-    </div>
-);
+        )
+    );
+    return (
+        <div className={styles.burgers}>
+            {burgers}
+        </div>
+    );
 }
 export default Burgers;
 
